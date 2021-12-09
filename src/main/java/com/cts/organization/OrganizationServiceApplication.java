56128d169d3d95d3d85fabdc9710b932b49d4028
navkk <navkk@host.docker.internal>
2021-12-09 10:17:00 +0530

    Changed properties significantly

--- src/main/java/com/cts/organization/OrganizationServiceApplication.java
+++ src/main/java/com/cts/organization/OrganizationServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.context.annotation.ComponentScan;
 
 @SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableBinding(Source.class)
+@ComponentScan()
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
